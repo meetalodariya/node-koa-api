aeea2ade694b60006115a29955a9268f7e83d7b9
meet8866 <meetalodariya45@gmail.com>
2020-04-14 23:04:53 +0530

    controller action for config

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,3 +1,9 @@
+const config = require("config");
+const configConsts = require("../constants");
+
 exports.getCheck = async (ctx) => {
-  ctx.body = "hi this is my app";
+  ctx.body = `
+  Server is running on: ${config.get(configConsts.PORT_NUM)}
+  URI : ${config.get(configConsts.URI)}
+  `;
 };
